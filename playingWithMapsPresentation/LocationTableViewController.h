dbbf7c4909ff501bf97a9a597f26652487baf125
Tprezioso <tommyprezioso@gmail.com>
2015-09-17 14:49:54 -0400

    added list table view controller and removed search bar textfield

---
+++ playingWithMapsPresentation/LocationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationTableViewController.h
+//  playingWithMapsPresentation
+//
+//  Created by Thomas Prezioso on 9/17/15.
+//  Copyright (c) 2015 Thomas Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationTableViewController : UITableViewController
+
+@end
