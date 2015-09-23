22cf3ef54eddb5e251f7c9a4cbaf0003c7651d83
Tprezioso <tommyprezioso@gmail.com>
2015-09-23 13:30:48 -0400

    added detail viewController

---
+++ playingWithMapsPresentation/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  playingWithMapsPresentation
+//
+//  Created by Thomas Prezioso on 9/23/15.
+//  Copyright © 2015 Thomas Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ playingWithMapsPresentation/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  playingWithMapsPresentation
+//
+//  Created by Thomas Prezioso on 9/23/15.
+//  Copyright © 2015 Thomas Prezioso. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *detailImageView;
+@property (strong, nonatomic) IBOutlet UILabel *detailTitleLabel;
+@property (strong, nonatomic) IBOutlet UITextView *detailTextView;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
