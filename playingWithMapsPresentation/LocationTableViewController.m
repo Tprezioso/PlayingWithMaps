ff1db4dc2e579a9745915c7e706e9463f7290856
Tprezioso <tommyprezioso@gmail.com>
2015-10-03 10:55:18 -0400

    refactoring some code

--- playingWithMapsPresentation/LocationTableViewController.m
+++ playingWithMapsPresentation/LocationTableViewController.m
@@ -77,13 +77,11 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
     DetailViewController *detailedVC = segue.destinationViewController;
     NSIndexPath *selectedIndexPath = [self.tableView indexPathForSelectedRow];
     detailedVC.detailLocations = self.locations[selectedIndexPath.row];
-    
 }
 
 
