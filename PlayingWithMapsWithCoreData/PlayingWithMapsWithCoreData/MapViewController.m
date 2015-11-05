07a4f18249d09260063dcc8f1a414b5dfd05cae8
Tprezioso <tommyprezioso@gmail.com>
2015-11-04 22:33:59 -0500

    another minor refactor

--- PlayingWithMapsWithCoreData/PlayingWithMapsWithCoreData/MapViewController.m
+++ PlayingWithMapsWithCoreData/PlayingWithMapsWithCoreData/MapViewController.m
@@ -5,6 +5,7 @@
 //  Created by Thomas Prezioso on 3/13/15.
 //  Copyright (c) 2015 Thomas Prezioso. All rights reserved.
 //
+
 #import "AppDelegate.h"
 #import "MapViewController.h"
 #import "DetailViewController.h"
