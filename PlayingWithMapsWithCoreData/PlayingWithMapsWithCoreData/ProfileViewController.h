ae9aab75fcf657f540a1a7d4b3c6dc9bf6c8bcbb
Tprezioso <tommyprezioso@gmail.com>
2015-11-12 15:48:04 -0500

    added profileVC

---
+++ PlayingWithMapsWithCoreData/PlayingWithMapsWithCoreData/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  PlayingWithMapsWithCoreData
+//
+//  Created by Thomas Prezioso on 11/12/15.
+//  Copyright © 2015 Thomas Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
